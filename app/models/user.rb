f85b70f782965bc88b1c382a0fd6b6b9b0da68cc
Linh Vu <linhvh.96@gmail.com>
2022-05-26 20:50:53 +0700

    user management

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
 
-  enum role: [:admin, :client]
+  enum role: [:admin_deel, :admin_client, :client]
 end
