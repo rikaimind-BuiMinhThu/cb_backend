67142d0ae975aff9fe8389676cda066ab7c95760
Ngoc Tuan <tuansaker1412@gmail.com>
2022-06-01 14:25:20 +0700

    skip cors

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem "ransack"
 gem "omniauth-facebook"
 gem "omniauth-rails_csrf_protection", "~> 1.0"
 gem "paranoia", "~> 2.6.0"
+gem "rack-cors"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module InstagramChatbot
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins /.*/
+        resource '*', headers: :any, methods: :any, credentials: true
+      end
+    end
   end
 end
