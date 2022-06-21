f56624a15cd566bdf586a86bb3199929ae0b4ccf
Linh Vu <linhvh.96@gmail.com>
2022-06-21 20:40:31 +0700

    chatbot

--- app/services/facebook_manager/chatbot_manager.rb
+++ app/services/facebook_manager/chatbot_manager.rb
@@ -26,7 +26,7 @@ module FacebookManager
       quick_replies.each do |quick_reply|
         request_body["quick_replies"].push({"content_type": "text", title: quick_reply})
       end
-      post_request "https://graph.facebook.com/v2.6/me/messages?access_token=#{@page_access_token}", request_body
+      post_request "https://graph.facebook.com/v2.6/me/messages?access_token=#{page_access_token}", request_body
     end
 
     private
