01b95443ca560983850e34ddbf6b23a4388bd10b
Linh Vu <linhvh.96@gmail.com>
2022-07-01 23:48:54 +0700

    hotfix token

--- app/controllers/api/v1/message_managements/ice_breakers_controller.rb
+++ app/controllers/api/v1/message_managements/ice_breakers_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::MessageManagements::IceBreakersController < ApplicationController
   skip_before_action :permision
   skip_before_action :verify_authenticity_token
 
+  IGACCESSTOKEN = InstagramAccount.find_by(ig_id: "17841453981073051").page_access_token
+
   def index
     render json: {code: 1, data: IceBreaker.all}
   end
@@ -32,7 +34,7 @@ class Api::V1::MessageManagements::IceBreakersController < ApplicationController
   end
 
   def status
-    instagram_ice_breakers = HttpManager.new("https://graph.facebook.com/v11.0/me/messenger_profile?fields=ice_breakers&platform=instagram&access_token=#{Settings.webhook.page_instagram_access_token}")
+    instagram_ice_breakers = HttpManager.new("https://graph.facebook.com/v11.0/me/messenger_profile?fields=ice_breakers&platform=instagram&access_token=#{IGACCESSTOKEN}")
       .get_request
     render json: {code: 1, instagram_ice_breakers: instagram_ice_breakers}
   end
@@ -43,7 +45,7 @@ class Api::V1::MessageManagements::IceBreakersController < ApplicationController
       call_to_actions.push({"question": ice_breaker.question, "payload": ice_breaker.answer})
     end
     instagram_ice_breaker = HttpManager.new(
-      "https://graph.facebook.com/v11.0/me/messenger_profile?platform=instagram&access_token=#{Settings.webhook.page_instagram_access_token}",
+      "https://graph.facebook.com/v11.0/me/messenger_profile?platform=instagram&access_token=#{IGACCESSTOKEN}",
       {
         "platform": "instagram",
         "ice_breakers": [
@@ -58,7 +60,7 @@ class Api::V1::MessageManagements::IceBreakersController < ApplicationController
 
   def turn_off
     instagram_ice_breaker = HttpManager.new(
-      "https://graph.facebook.com/v11.0/me/messenger_profile?platform=instagram&access_token=#{Settings.webhook.page_instagram_access_token}",
+      "https://graph.facebook.com/v11.0/me/messenger_profile?platform=instagram&access_token=#{IGACCESSTOKEN}",
       {
         "fields": [
           "ice_breakers"
