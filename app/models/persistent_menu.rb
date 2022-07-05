a94b1de9a3df189f5881249c8d2f83848e8f9b88
Ngoc Tuan <tuansaker1412@gmail.com>
2022-07-05 21:23:35 +0700

    update

--- app/models/persistent_menu.rb
+++ app/models/persistent_menu.rb
@@ -1,3 +1,10 @@
 class PersistentMenu < ApplicationRecord
   belongs_to :instagram_account
+
+  private
+
+  def self.validate_size!(instagram_account_id)
+    return false if PersistentMenu.where(instagram_account_id: instagram_account_id).size >= 5
+    return true
+  end
 end
