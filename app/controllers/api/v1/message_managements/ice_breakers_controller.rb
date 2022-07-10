1934ff3a017c83a626ee0d1637ee7539fb42c6de
Linh Vu <linhvh.96@gmail.com>
2022-07-10 12:11:46 +0700

    double render

--- app/controllers/api/v1/message_managements/ice_breakers_controller.rb
+++ app/controllers/api/v1/message_managements/ice_breakers_controller.rb
@@ -59,7 +59,7 @@ class Api::V1::MessageManagements::IceBreakersController < ApplicationController
     IceBreaker.where(instagram_account: current_user.instagram_account).each do |ice_breaker|
       call_to_actions.push({"question": ice_breaker.question, "payload": ice_breaker.answer})
     end
-    render json: {code: 2, instagram_persistent_menu: "ice_breaker is blank"} if call_to_actions.blank?
+    return render json: {code: 2, instagram_persistent_menu: "ice_breaker is blank"} if call_to_actions.blank?
     instagram_ice_breaker = HttpManager.new(
       "https://graph.facebook.com/v11.0/me/messenger_profile?platform=instagram&access_token=#{ig_access_token}",
       {
