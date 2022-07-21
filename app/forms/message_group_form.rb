d9923b338c4fafc453bb9a1662b963ec6ea4a6d6
Ngoc Tuan <tuansaker1412@gmail.com>
2022-07-21 10:50:44 +0700

    update

--- app/forms/message_group_form.rb
+++ app/forms/message_group_form.rb
@@ -13,12 +13,12 @@ class MessageGroupForm
     return false if @instagram_account.blank?
     list_bag_ids = @message_group.message_bags.pluck(:id)
     if list_bag_ids.present?
-      if instagram_account.post_comment_bag_id.present?
-        return false if list_bag_ids.include?(instagram_account.post_comment_bag_id)
-      elsif  instagram_account.story_comment_bag_id.present?
-        return false if list_bag_ids.include?(instagram_account.story_comment_bag_id)
-      elsif instagram_account.live_comment_bag_id.present?
-        return false if list_bag_ids.include?(instagram_account.live_comment_bag_id)
+      if @instagram_account.post_comment_bag_id.present?
+        return false if list_bag_ids.include?(@instagram_account.post_comment_bag_id)
+      elsif  @instagram_account.story_comment_bag_id.present?
+        return false if list_bag_ids.include?(@instagram_account.story_comment_bag_id)
+      elsif @instagram_account.live_comment_bag_id.present?
+        return false if list_bag_ids.include?(@instagram_account.live_comment_bag_id)
       end
     end
     return true
