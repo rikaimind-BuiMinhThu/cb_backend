e40f71f3af7021ad7993c1f9459e2bd4a2af266c
Linh Vu <linhvh.96@gmail.com>
2022-07-31 21:54:23 +0700

    fix db

--- db/migrate/20220731091712_create_instagram_user_labels.rb
+++ db/migrate/20220731091712_create_instagram_user_labels.rb
@@ -2,7 +2,7 @@ class CreateInstagramUserLabels < ActiveRecord::Migration[7.0]
   def change
     create_table :instagram_user_labels do |t|
       t.references :instagram_user, null: false, foreign_key: true
-      t.references :message_button_labels, null: false, foreign_key: true
+      t.references :message_button_label, null: false, foreign_key: true
 
       t.timestamps
     end
