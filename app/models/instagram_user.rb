22566426f18259c662b0d1e4d58b33c208e78cae
Linh Vu <linhvh.96@gmail.com>
2022-07-31 21:46:09 +0700

    label

--- app/models/instagram_user.rb
+++ app/models/instagram_user.rb
@@ -1,6 +1,6 @@
 class InstagramUser < ApplicationRecord
-  has_many :instagram_user_message_button_labels
-  has_many :message_button_labels, through: :instagram_user_message_button_labels
+  has_many :instagram_user_labels
+  has_many :message_button_labels, through: :instagram_user_labels
   belongs_to :instagram_account
   has_many :chatbot_usages
 end
