271683af12d8c0df692b0ef3a142c9c651c7838f
Ngoc Tuan <tuansaker1412@gmail.com>
2022-08-05 01:00:10 +0700

    update

--- app/controllers/api/v1/chatbots_controller.rb
+++ app/controllers/api/v1/chatbots_controller.rb
@@ -147,7 +147,7 @@ class Api::V1::ChatbotsController < ApplicationController
       message_button_labels = MessageButton.find_by(id: message_button_id)&.message_button_labels
       if message_button_labels.present?
         message_button_labels.each do |message_button_label|
-          InstagramUserLabel.find_or_create_by(message_button_label: message_button_label, instagram_user: instagram_user)
+          InstagramUserLabel.find_or_create_by(name: message_button_label.label_name, instagram_user: instagram_user)
         end
       end
     end
--- app/views/api/v1/managements/instagram_users/index.json.jbuilder
+++ app/views/api/v1/managements/instagram_users/index.json.jbuilder
@@ -2,6 +2,8 @@ json.code 1
 json.data do
   json.instagram_users @instagram_users.each do |instagram_user|
     json.merge! instagram_user.as_json
-    json.labels instagram_user.message_button_labels.pluck(:label_name)
+    # json.labels instagram_user.instagram_user_labels.select(:id, :name)
+    # json.custom_items instagram_user.custom_items.select(:id, :title, :value)
+    json.updated_at instagram_user.chatbot_usages.last.updated_at if instagram_user.chatbot_usages.last.present?
   end
 end
