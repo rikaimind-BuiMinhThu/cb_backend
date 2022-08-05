b4b5df36b55c30af0d4e3b9f066de5408dcbf1a0
Ngoc Tuan <tuansaker1412@gmail.com>
2022-08-05 19:03:29 +0700

    fix

--- app/controllers/api/v1/managements/users_controller.rb
+++ app/controllers/api/v1/managements/users_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::Managements::UsersController < ApplicationController
     return render json: {code: 2, data: "Not have permission"} if current_user.client?
     q = {full_name_or_email_cont: params[:name], client_id_eq: params[:client_id]} if current_user.admin_deel?
     q = {full_name_or_email_cont: params[:name], client_id_eq: current_user.client_id} if current_user.admin_client?
-    q[:user_labels_label_eq] = params[:user_labels]
     @users = User.ransack(q).result
     @total = @users.size
     @users = @users.page(params[:page])
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   has_many :identities
   has_one :instagram_account, dependent: :destroy
   has_many :message_groups, dependent: :destroy
-  has_many :user_labels, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable, :trackable,
