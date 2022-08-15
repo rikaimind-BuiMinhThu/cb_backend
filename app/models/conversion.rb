4022257911407ac02f602cd929f5214f63359347
Linh Vu <linhvh.96@gmail.com>
2022-08-16 00:51:51 +0700

    fix conversion

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,6 +1,6 @@
 class Conversion < ApplicationRecord
   belongs_to :instagram_user
-  belongs_to :message_bag_id
+  belongs_to :message_bag
 
   enum user_source: {dm: 0, post_comment: 1, story_comment: 2, live_comment: 3}
 end
