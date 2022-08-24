6520a84359b42ee990608bc56811d175b8a16e90
Ngoc Tuan <tuansaker1412@gmail.com>
2022-08-24 23:12:58 +0700

    update

--- app/controllers/api/v1/message_managements/keyword_settings_controller.rb
+++ app/controllers/api/v1/message_managements/keyword_settings_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::MessageManagements::KeywordSettingsController < ApplicationContro
 
   def index
     instagram_account_ids = InstagramAccount.where(user: current_user).pluck(:id)
-    @keywords = KeywordSetting.where(instagram_account_id: instagram_account_ids)
+    @keywords = KeywordSetting.where(instagram_account_id: instagram_account_ids).includes(:message_bag)
     # render json: {code: 1, data: keywords}
   end
 
