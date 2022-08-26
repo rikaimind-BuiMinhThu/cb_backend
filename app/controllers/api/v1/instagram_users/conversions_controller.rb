a5a4fc95249b3f007110e9df96ece3b11a8cc006
Linh Vu <linhvh.96@gmail.com>
2022-08-27 00:45:47 +0700

    conversion

--- app/controllers/api/v1/instagram_users/conversions_controller.rb
+++ app/controllers/api/v1/instagram_users/conversions_controller.rb
@@ -7,8 +7,8 @@ class Api::V1::InstagramUsers::ConversionsController < ApplicationController
     return render json: {code: 2, data: "Not have permission"} unless current_user.admin_deel? || current_user.admin_client?
     instagram_users = instagram_users.where(instagram_account: current_user.instagram_account)
     live_instagram_users = instagram_users.live_comment
-    story_instagram_users = instagram_user.story_comment
-    dm_instagram_users = instagram_user.dm
+    story_instagram_users = instagram_users.story_comment
+    dm_instagram_users = instagram_users.dm
 
     @live_instagram_user_count = live_instagram_users.count
     @story_instagram_user_count = story_instagram_users.count
@@ -18,16 +18,16 @@ class Api::V1::InstagramUsers::ConversionsController < ApplicationController
     @story_instagram_message_count = ChatbotUsage.where(instagram_user: story_instagram_users.pluck(:id)).count
     @dm_instagram_message_count = ChatbotUsage.where(instagram_user: dm_instagram_users.pluck(:id)).count
 
-    @live_conversion_count = Conversion.where(instagram_user: live_instagram_users.pluck(:id))
-    @story_conversion_count = Conversion.where(instagram_user: story_instagram_users.pluck(:id))
-    @dm_conversion_count = Conversion.where(instagram_user: dm_instagram_users.pluck(:id))
+    @live_conversion_count = Conversion.where(instagram_user: live_instagram_users.pluck(:id)).count
+    @story_conversion_count = Conversion.where(instagram_user: story_instagram_users.pluck(:id)).count
+    @dm_conversion_count = Conversion.where(instagram_user: dm_instagram_users.pluck(:id)).count
   end
 
   def show
     instagram_user = InstagramUser.find_by(id: params[:id])
     return render json: {code: 2, data: "Not found"} if instagram_user.blank?
     return render json: {code: 2, data: "Not have permission"} unless current_user.admin_deel? || (current_user.admin_client? && instagram_user.instagram_account == current_user.instagram_account)
-    @conversions = instagram_user.conversions
+    @conversion_count = instagram_user.conversions.count
     @instagram_message_count = instagram_user.chatbot_usage.count
   end
 
