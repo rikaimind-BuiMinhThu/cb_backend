334fe6a7196c7ac2b031b69b1c21f81057e7a089
Linh Vu <linhvh.96@gmail.com>
2022-08-27 00:47:33 +0700

    fix create conversion

--- app/controllers/api/v1/instagram_users/conversions_controller.rb
+++ app/controllers/api/v1/instagram_users/conversions_controller.rb
@@ -34,11 +34,13 @@ class Api::V1::InstagramUsers::ConversionsController < ApplicationController
   def create
     instagram_user = InstagramUser.find_by(id: params[:instagram_user_id])
     return render json: {code: 2, data: "Not found"} if instagram_user.blank?
+    message_bag = MessageBag.find_by(id: params[:instagram_user_id])
+    return render json: {code: 2, data: "Not found"} if message_bag.blank?
     conversion = Conversion.new(instagram_user: instagram_user,
                       user_name: instagram_user.username,
                       user_source: instagram_user.start_chatbot_in,
                       conversion_at: Time.current,
-                      message_bag_id: params[:message_bag_id])
+                      message_bag_id: message_bag.id)
     return render json: {code: 1, data: "success"} if conversion.save
     render json: {code: 2, data: conversion.errors.full_messages}
   end
