56e2e3048f0333482c8694de0b304a262d7340f3
Linh Vu <linhvh.96@gmail.com>
2022-08-27 00:51:21 +0700

    fix create conversion

--- app/controllers/api/v1/instagram_users/conversions_controller.rb
+++ app/controllers/api/v1/instagram_users/conversions_controller.rb
@@ -28,13 +28,13 @@ class Api::V1::InstagramUsers::ConversionsController < ApplicationController
     return render json: {code: 2, data: "Not found"} if instagram_user.blank?
     return render json: {code: 2, data: "Not have permission"} unless current_user.admin_deel? || (current_user.admin_client? && instagram_user.instagram_account == current_user.instagram_account)
     @conversion_count = instagram_user.conversions.count
-    @instagram_message_count = instagram_user.chatbot_usage.count
+    @instagram_message_count = instagram_user.chatbot_usages.count
   end
 
   def create
     instagram_user = InstagramUser.find_by(id: params[:instagram_user_id])
     return render json: {code: 2, data: "Not found"} if instagram_user.blank?
-    message_bag = MessageBag.find_by(id: params[:instagram_user_id])
+    message_bag = MessageBag.find_by(id: params[:message_bag_id])
     return render json: {code: 2, data: "Not found"} if message_bag.blank?
     conversion = Conversion.new(instagram_user: instagram_user,
                       user_name: instagram_user.username,
