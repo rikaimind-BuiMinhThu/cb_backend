b84e3d40655abd434a02f4dcabf588cde3ceafe6
Linh Vu <linhvh.96@gmail.com>
2022-08-28 11:30:18 +0700

    validate group name

--- app/models/message_group.rb
+++ app/models/message_group.rb
@@ -3,4 +3,6 @@ class MessageGroup < ApplicationRecord
   has_many :message_bags, dependent: :destroy
   has_many :chatbot_usage_groups
   belongs_to :user
+
+  validates :group_name, presence: true, uniqueness: true
 end
