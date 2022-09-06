ace3f3a7235869313e0ea0a2e7a66fb7f9f62956
Ngoc Tuan <tuansaker1412@gmail.com>
2022-09-06 22:58:18 +0700

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ Rails.application.routes.draw do
         get "/persistent_menus_turn_off" => "persistent_menus#turn_off"
         resources :keyword_settings, except: [:new, :edit]
         get "/keyword_settings_active" => "keywords#active"
-        resources :hot_templates, only: [:index, :create]
+        resources :hot_templates, except: [:new, :edit, :show]
       end
       namespace :instagram_users do
         resources :custom_items, except: [:index, :new, :edit]
