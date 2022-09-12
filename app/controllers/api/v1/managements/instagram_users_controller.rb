a43cdbbbd1e950093f62c4c302b8bea3e1bde162
Ngoc Tuan <tuansaker1412@gmail.com>
2022-09-13 00:22:10 +0700

    add paging to some api

--- app/controllers/api/v1/managements/instagram_users_controller.rb
+++ app/controllers/api/v1/managements/instagram_users_controller.rb
@@ -16,6 +16,7 @@ class Api::V1::Managements::InstagramUsersController < ApplicationController
     end
 
     @instagram_users = @instagram_users.where(instagram_account: current_user.instagram_account) if current_user.admin_client?
+    @instagram_users = @instagram_users.page(params[:page])
   end
 
   def show
