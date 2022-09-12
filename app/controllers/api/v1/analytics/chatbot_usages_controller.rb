375d61ae12e019df46cfce37fcfc92a3265a0b18
Linh Vu <linhvh.96@gmail.com>
2022-09-12 23:06:24 +0700

    fix datee

--- app/controllers/api/v1/analytics/chatbot_usages_controller.rb
+++ app/controllers/api/v1/analytics/chatbot_usages_controller.rb
@@ -5,10 +5,10 @@ class Api::V1::Analytics::ChatbotUsagesController < ApplicationController
     return render json: {code: 2, message: "Missing begin date"} if params[:begin_date].blank?
     return render json: {code: 2, message: "Missing end date"} if params[:end_date].blank?
 
-    begin_date = params[:begin_date].to_date.beginning_of_day
-    end_date = params[:end_date].to_date.end_of_day
+    begin_date = params[:begin_date].to_date
+    end_date = params[:end_date].to_date
 
-    @q = {created_at_lteq: end_date, created_at_gteq: begin_date}
+    @q = {created_at_lteq: end_date.end_of_day, created_at_gteq: begin_date.beginning_of_day}
     @q[:instagram_account_eq] = current_user.instagram_account
     return get_stats_live if params[:id] == "live"
     if params[:id] == "user"
