0be6ea44b89fa0c42b258568c3e97c4c1761582e
Ngoc Tuan <tuansaker1412@gmail.com>
2022-09-23 09:34:31 +0700

    update

--- app/models/chatbot.rb
+++ app/models/chatbot.rb
@@ -4,7 +4,7 @@ class Chatbot < ApplicationRecord
   belongs_to :user
 
   enum design_type: {pop: 0, flat: 1, material: 2}
-  enum main_color: {red: 0, yellow: 1, orange: 2, blue: 3, green: 4, purple: 5, black: 6, white: 7}
+  enum main_color: {pink: 0, yellow: 1, orange: 2, blue: 3, green: 4, purple: 5, black: 6, white: 7}
   enum status: {off: 0, on: 1}, _prefix: true
 
   mount_base64_uploader :icon, ChatbotIconUploader
