add30c6f3867ef12459635b357899421d677760c
Linh Vu <linhvh.96@gmail.com>
2022-12-13 21:58:58 +0700

    senario page

--- app/models/scenario.rb
+++ app/models/scenario.rb
@@ -1,6 +1,7 @@
 class Scenario < ApplicationRecord
   belongs_to :chatbot
   has_many :analytic_scenarios
+  has_many :scenario_pages
 
   validates :name, presence: true, uniqueness: { scope: :chatbot }
 end
