b9a700ea44e6c4f30663b6659ae8482ccc41106b
Linh Vu <linhvh.96@gmail.com>
2022-12-16 23:14:09 +0700

    download senario count

--- app/controllers/api/v1/analytics/scenario_counts_controller.rb
+++ app/controllers/api/v1/analytics/scenario_counts_controller.rb
@@ -25,4 +25,53 @@ class Api::V1::Analytics::ScenarioCountsController < ApplicationController
     return render json: {code: 1, message: "Success"} if analytic_scenario.save
     render json: {code: 2, message: analytic_scenario.errors.full_messages}
   end
+
+  def download
+    return render json: {code: 2, data: "No permission"} unless current_user.admin_deel? || current_user.admin_client?
+    scenario = Scenario.find_by(id: params[:id])
+    return render json: {code: 2, message: "Cannot find scenario"} if scenario.blank?
+    begin_date = params[:begin_date].to_date
+    end_date = params[:end_date].to_date
+
+    q = {created_at_lteq: end_date, created_at_gteq: begin_date}
+    analytic_scenarios = scenario.analytic_scenarios.ransack(q).result
+
+
+    analytic_scenarios = analytic_scenarios.group("DATE_FORMAT(created_at, '%d/%m/%Y')")
+                                           .select(generate_analytic_scenario_count_query)
+
+
+    date_arr = create_date_arr(begin_date, end_date)
+    analytic_scenarios.each do |date_hash|
+      AnalyticScenario.type_of_analytics.each do |key, value|
+        date_arr.map { |x| x[(key + "_count").to_sym] = (x[:log_date] == date_hash.log_date) ? date_hash[(key + "_count")] : x[(key + "_count").to_sym] }
+      end
+    end
+    render json: {code: 1, data: date_arr}
+  end
+
+  private
+
+  def create_date_arr(start_date, end_date)
+    date_arr = []
+    (start_date..end_date).each do |datee|
+      date_hash = {
+        log_date: datee.strftime("%d/%m/%Y")
+      }
+      AnalyticScenario.type_of_analytics.each do |key, value|
+        date_hash[(key + "_count").to_sym] = 0
+      end
+      date_arr.push(date_hash)
+    end
+    date_arr
+  end
+
+  def generate_analytic_scenario_count_query
+    query = "DATE_FORMAT(created_at, '%d/%m/%Y') as log_date,"
+    AnalyticScenario.type_of_analytics.each do |key, value|
+      query += "COUNT(CASE WHEN type_of_analytic = #{value} THEN 1 END) AS #{key}_count,"
+    end
+    #remove last ','
+    query[0...-1]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -76,6 +76,7 @@ Rails.application.routes.draw do
         resources :users, only: :index
         resources :chatbot_usages, only: :show
         resources :scenario_counts, only: [:show, :update]
+        get "scenario_counts/:id/download" => "scenario_counts#download"
         resources :scenario_pages, only: [:show, :create]
       end
       resources :prefectures, only: :index
