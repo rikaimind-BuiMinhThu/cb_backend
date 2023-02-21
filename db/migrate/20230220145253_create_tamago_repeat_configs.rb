186ee0ef04cc27f268043aad966e819fe089815b
Tuan Dang <dangtuan030690@gmail.com>
2023-02-21 09:49:57 +0700

    update migrate

--- db/migrate/20230220145253_create_tamago_repeat_configs.rb
+++ db/migrate/20230220145253_create_tamago_repeat_configs.rb
@@ -2,7 +2,7 @@ class CreateTamagoRepeatConfigs < ActiveRecord::Migration[7.0]
   def change
     create_table :tamago_repeat_configs do |t|
       t.integer :scenario_id
-      t.text :tamago_landingPgae_url
+      t.text :tamago_landing_page_url
       t.text :add_to_cart_button_selector
       t.boolean :is_regular_product
       t.integer :email_confirm_field
