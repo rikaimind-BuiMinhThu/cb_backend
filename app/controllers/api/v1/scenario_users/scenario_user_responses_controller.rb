fbe1e8bc1fae06ea0436e8fbf0aed373b0736ef0
Tuan Dang <dangtuan030690@gmail.com>
2023-02-26 05:57:30 +0700

    udpate condition

--- app/controllers/api/v1/scenario_users/scenario_user_responses_controller.rb
+++ app/controllers/api/v1/scenario_users/scenario_user_responses_controller.rb
@@ -1,17 +1,36 @@
 class Api::V1::ScenarioUsers::ScenarioUserResponsesController < ApplicationController
   skip_before_action :permision
   skip_before_action :verify_authenticity_token
+  before_action :set_scenario
 
   def create
-    scenario_user_response = ScenarioUserResponse.build_record(params)
-    scenario_user_response.save
-    render json: {code: 1, data: scenario_user_response}
+    if @client.tamago_repeat?
+      scenario_user_response = ScenarioUserResponse.build_record(params)
+      scenario_user_response.save
+      render json: {code: 1, data: scenario_user_response}
+    else
+      render json: {code: 1, data: []}
+    end
   end
 
   def create_order
-    scenario = Scenario.find_by(params[:scenario_id])
-    conversations = scenario.scenario_user_responses.where(user_input_id: params[:user_id])
-    service = TamagoScenario::SeleniumService.new(scenario, conversations)
-    service.process
+    if @client.tamago_repeat?
+      begin
+        conversations = @scenario.scenario_user_responses.where(user_input_id: params[:user_id])
+        service = TamagoScenario::SeleniumService.new(@scenario, conversations)
+        service.process
+      rescue StandardError => e
+
+      end
+    else
+      render json: {code: 1, message: 'not create order'}
+    end
+  end
+
+  private
+
+  def set_scenario
+    @scenario = Scenario.find_by(params[:scenario_id])
+    @client = @scenario.chatbot&.user&.client
   end
 end
