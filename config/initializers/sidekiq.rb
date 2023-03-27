06925072a37ef7e4b32f71cc04a6ee2da826c927
ThuBui <thu.bui@rikai.technology>
2023-03-28 03:13:52 +0900

    Add sidekiq log

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
+  config.logger = Rails.logger
+end
+
+Sidekiq.configure_server do |config|
+  config.logger = Rails.logger
 end
