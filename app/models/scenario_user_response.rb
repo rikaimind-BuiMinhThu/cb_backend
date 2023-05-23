7a8b62f62d1db53f59bf02abceaa11c56b8536ac
Tuan Dang <dangtuan030690@gmail.com>
2023-05-24 04:11:27 +0700

    add paidy payment

--- app/models/scenario_user_response.rb
+++ app/models/scenario_user_response.rb
@@ -91,139 +91,152 @@ class ScenarioUserResponse < ApplicationRecord
     built_result = []
 
     puts "-----------------------------------------------------"
-    params[:message][:message_content].each do |conversation|
-      puts "-----------------------------conversion: #{conversation[:type]}"
-      data_input_name = nil
-      value = nil
-
-      case conversation[:type]
-      when "text_input"
-        puts "-----------------------------conversion: #{conversation[:text_input][:save_input_content]}"
-        case conversation[:text_input][:save_input_content]
-        when "user_email"
-          data_input_name = "user_email"
-          value = conversation.dig(:text_input, :email_address, :value)
-        when "user_name"
-          data_input_name = "user_name"
-          value = conversation[:text_input][:text].to_json
-        when "user_name_kana"
-          data_input_name = "user_name_kana"
-          value = conversation[:text_input][:text].to_json
-        when "first_name_kana"
-          data_input_name = "first_name_kana"
-          value = conversation.dig(:text_input, :text, :value)
-        when "last_name_kana"
-          data_input_name = "last_name_kana"
-          value = conversation.dig(:text_input, :text, :value)
-        when "phone_number"
-          data_input_name = "phone_number"
-          value = conversation.dig(:text_input, :phone_number, :value)
-        when "password"
-          data_input_name = "user_password"
-          value = conversation.dig(:text_input, :password_confirmation, :value)
-        when "quantity"
-          data_input_name = "quantity"
-          value = conversation.dig(:text_input, :text, :value)
-        when "last_name"
-          data_input_name = "last_name"
-          value = conversation.dig(:text_input, :text, :value)
-        when "first_name"
-          data_input_name = "first_name"
-          value = conversation.dig(:text_input, :text, :value)
-        when "coupons_code"
-          data_input_name = "coupons_code"
-          value = conversation.dig(:text_input, :text, :value)
-        end
-      when "zip_code_address"
-        data_input_name = "zip_code_address"
-        value = conversation[:zip_code_address].to_json
-      when "radio_button"
-        case conversation[:radio_button][:save_input_content]
-        when "is_regular_order"
-          value = conversation[:radio_button][:initial_selection] == 1
-          data_input_name = "is_regular_order"
-        when "has_account"
-          selected = get_selected_obj_for_radio_button(conversation)
-          value = selected[:value]
-          data_input_name = "has_account"
-        when "delivery_frequency"
-          selected = get_selected_obj_for_radio_button(conversation)
-          value = selected[:value]
-          data_input_name = "delivery_frequency"
-        when "delivery_method"
-          selected = get_selected_obj_for_radio_button(conversation)
-          value = selected[:value]
-          data_input_name = "delivery_method"
-        when "payment_method"
-          selected = get_selected_obj_for_radio_button(conversation)
-          value = selected[:value]
-          data_input_name = "payment_method"
-        when "sex"
-          selected = get_selected_obj_for_radio_button(conversation)
-          value = selected[:value]
-          data_input_name = "sex"
-        end
-      when "card_payment_radio_button"
-        selected = get_selected_obj_for_card_payment_radio_button(conversation)
-
-        if conversation[:card_payment_radio_button][:initial_selection] == conversation[:card_payment_radio_button][:card_linked_setting]
-          data_input_name = "credit_card_payment"
-          value = conversation[:card_payment_radio_button].to_json
-        elsif selected[:value] == 'paypal'
-          data_input_name = "paypal_payment"
-          value = conversation[:card_payment_radio_button].to_json
-        elsif selected[:value] == 'komoju'
-          data_input_name = "komoju_payment"
-          value = conversation[:card_payment_radio_button].to_json
-        else
-          data_input_name = "np_delivery_payment"
-          value = selected[:value]
-        end
-      when "pull_down"
-        case conversation[:pull_down][:save_input_content]
-        when "birthday" # 誕生日
-          value = conversation[:pull_down][:dob_ymd].to_json
-          data_input_name = "birth_date"
-        when "delivery_date" # お届け希望日
-          value = get_selected_value_for_pull_down(conversation)
-          data_input_name = "delivery_date"
-        when "delivery_time" # 時間帯指定
-          value = get_selected_value_for_pull_down(conversation)
-          data_input_name = "delivery_time"
-        when "quantity" # 数量
-          selected = get_selected_value_for_pull_down(conversation)
-          value = selected.to_i
-          data_input_name = "quantity"
-        when "delivery_method"
-          selected = get_selected_value_for_pull_down(conversation)
-          value = selected.to_i
-          data_input_name = "delivery_method"
-        when "country"
-          data_input_name = "country"
-          value = conversation[:pull_down][:customization][:options_without_comment][0][:value]
-        end
-    when "textarea"
-      puts "-----------------------------conversion: #{conversation[:textarea][:save_input_content]}"
-      case conversation[:textarea][:save_input_content]
-      when "sent_message"
-        data_input_name = "sent_message"
-        value = conversation[:textarea][:text_input][:value]
+    if params[:message][:conditions].present?
+      scenario = Scenario.find(scenario_id)
+      conversations = scenario.scenario_user_responses.where(user_input_id: user_id)
+      condition = params[:message][:conditions].first
+      if condition['inputCondition'] == 'paypal'
+        user_response = conversations.find_by(data_input_name: 'paypal_payment')
+      elsif condition['inputCondition'] == 'paidy'
+        user_response = conversations.find_by(data_input_name: 'paidy_payment')
       end
-    end
-      puts "=============================="
-      puts "data_input_name: #{data_input_name}"
-      next unless data_input_name.present?
-      new_record = self.new(
-        scenario_id: scenario_id,
-        user_input_id: user_id,
-        data_input_name: data_input_name,
-        value: value,
-      )
+      user_response.update(value: params[:message][:message_content].to_json)
+    else
+      params[:message][:message_content].each do |conversation|
+        puts "-----------------------------conversion: #{conversation[:type]}"
+        data_input_name = nil
+        value = nil
 
-      built_result.push(new_record)
-    end
+        case conversation[:type]
+        when "text_input"
+          puts "-----------------------------conversion: #{conversation[:text_input][:save_input_content]}"
+          case conversation[:text_input][:save_input_content]
+          when "user_email"
+            data_input_name = "user_email"
+            value = conversation.dig(:text_input, :email_address, :value)
+          when "user_name"
+            data_input_name = "user_name"
+            value = conversation[:text_input][:text].to_json
+          when "user_name_kana"
+            data_input_name = "user_name_kana"
+            value = conversation[:text_input][:text].to_json
+          when "first_name_kana"
+            data_input_name = "first_name_kana"
+            value = conversation.dig(:text_input, :text, :value)
+          when "last_name_kana"
+            data_input_name = "last_name_kana"
+            value = conversation.dig(:text_input, :text, :value)
+          when "phone_number"
+            data_input_name = "phone_number"
+            value = conversation.dig(:text_input, :phone_number, :value)
+          when "password"
+            data_input_name = "user_password"
+            value = conversation.dig(:text_input, :password_confirmation, :value)
+          when "quantity"
+            data_input_name = "quantity"
+            value = conversation.dig(:text_input, :text, :value)
+          when "last_name"
+            data_input_name = "last_name"
+            value = conversation.dig(:text_input, :text, :value)
+          when "first_name"
+            data_input_name = "first_name"
+            value = conversation.dig(:text_input, :text, :value)
+          when "coupons_code"
+            data_input_name = "coupons_code"
+            value = conversation.dig(:text_input, :text, :value)
+          end
+        when "zip_code_address"
+          data_input_name = "zip_code_address"
+          value = conversation[:zip_code_address].to_json
+        when "radio_button"
+          case conversation[:radio_button][:save_input_content]
+          when "is_regular_order"
+            value = conversation[:radio_button][:initial_selection] == 1
+            data_input_name = "is_regular_order"
+          when "has_account"
+            selected = get_selected_obj_for_radio_button(conversation)
+            value = selected[:value]
+            data_input_name = "has_account"
+          when "delivery_frequency"
+            selected = get_selected_obj_for_radio_button(conversation)
+            value = selected[:value]
+            data_input_name = "delivery_frequency"
+          when "delivery_method"
+            selected = get_selected_obj_for_radio_button(conversation)
+            value = selected[:value]
+            data_input_name = "delivery_method"
+          when "payment_method"
+            selected = get_selected_obj_for_radio_button(conversation)
+            value = selected[:value]
+            data_input_name = "payment_method"
+          when "sex"
+            selected = get_selected_obj_for_radio_button(conversation)
+            value = selected[:value]
+            data_input_name = "sex"
+          end
+        when "card_payment_radio_button"
+          selected = get_selected_obj_for_card_payment_radio_button(conversation)
 
-    built_result
+          if conversation[:card_payment_radio_button][:initial_selection] == conversation[:card_payment_radio_button][:card_linked_setting]
+            data_input_name = "credit_card_payment"
+            value = conversation[:card_payment_radio_button].to_json
+          elsif selected[:value] == 'paypal'
+            data_input_name = "paypal_payment"
+          elsif selected[:value] == 'komoju'
+            data_input_name = "komoju_payment"
+            value = conversation[:card_payment_radio_button].to_json
+          elsif selected[:value] == 'paidy'
+            data_input_name = "paidy_payment"
+          else
+            data_input_name = "np_delivery_payment"
+            value = selected[:value]
+          end
+        when "pull_down"
+          case conversation[:pull_down][:save_input_content]
+          when "birthday" # 誕生日
+            value = conversation[:pull_down][:dob_ymd].to_json
+            data_input_name = "birth_date"
+          when "delivery_date" # お届け希望日
+            value = get_selected_value_for_pull_down(conversation)
+            data_input_name = "delivery_date"
+          when "delivery_time" # 時間帯指定
+            value = get_selected_value_for_pull_down(conversation)
+            data_input_name = "delivery_time"
+          when "quantity" # 数量
+            selected = get_selected_value_for_pull_down(conversation)
+            value = selected.to_i
+            data_input_name = "quantity"
+          when "delivery_method"
+            selected = get_selected_value_for_pull_down(conversation)
+            value = selected.to_i
+            data_input_name = "delivery_method"
+          when "country"
+            data_input_name = "country"
+            value = conversation[:pull_down][:customization][:options_without_comment][0][:value]
+          end
+      when "textarea"
+        puts "-----------------------------conversion: #{conversation[:textarea][:save_input_content]}"
+        case conversation[:textarea][:save_input_content]
+        when "sent_message"
+          data_input_name = "sent_message"
+          value = conversation[:textarea][:text_input][:value]
+        end
+      end
+        puts "=============================="
+        puts "data_input_name: #{data_input_name}"
+        next unless data_input_name.present?
+        new_record = self.new(
+          scenario_id: scenario_id,
+          user_input_id: user_id,
+          data_input_name: data_input_name,
+          value: value,
+        )
+
+        built_result.push(new_record)
+      end
+
+      built_result
+    end
   end
  
   
@@ -252,7 +265,6 @@ class ScenarioUserResponse < ApplicationRecord
   integer :sex, is_encrypt: false
   text :birth_date, is_encrypt: false
   text :credit_card_payment, is_encrypt: true
-  text :paypal_payment, is_encrypt: false
   string :cash_on_delivery_payment, is_encrypt: false
   string :np_delivery_payment, is_encrypt: false
   boolean :is_regular_order
@@ -263,4 +275,5 @@ class ScenarioUserResponse < ApplicationRecord
   string :delivery_time, is_encrypt: false
   text :komoju_payment, is_encrypt: true
   text :paypal_payment, is_encrypt: true
+  text :paidy_payment, is_encrypt: false
 end
--- app/services/selenium_services/shopify.rb
+++ app/services/selenium_services/shopify.rb
@@ -27,7 +27,9 @@ module SeleniumServices
     SHOPIFY_PAYMENT_EXPIRY_DATE_INPUT = "input#expiry"
     SHOPIFY_PAYMENT_SECURITY_CODE_INPUT = "input#verification_value"
 
-    PAYPAL_PAYMENT_CREDIT_CARD_LABEL = "label[for=basic-PAYPAL_EXPRESS]"
+    PAYPAL_PAYMENT_LABEL = "label[for=basic-PAYPAL_EXPRESS]"
+    PAYPAL_PAYMENT_WITH_ANOTHER_METHOD = "a[class=scTrack:tryAnotherWayLink]"
+    PAYPAL_PAYMENT_WITH_ANOTHER_METHOD_BUTTON = "a[id=loginWithPassword]"
     PAYPAL_PAYMENT_BUTTON = "div[id=buttons-container][aria-label=PayPal]"
     PAYPAL_PAYMENT_EMAIL_INPUT = "input#email"
     PAYPAL_PAYMENT_NEXT_BUTTON = "button#btnNext"
@@ -41,6 +43,18 @@ module SeleniumServices
     KOMOJU_PAYMENT_EXPIRY_DATE_INPUT = "input#expiration"
     KOMOJU_PAYMENT_SECURITY_CODE_INPUT = "input#verification"
     KOMOJU_PAY_NOW_BUTTON = "input[type=submit]"
+
+    PAIDY_PAYMENT_LABEL = "label[for=basic-あと払い（ペイディ）]"
+    PAIDY_PAYMENT_EMAIL_INPUT = "input#ip_email"
+    PAIDY_PAYMENT_PHONE_INPUT = "input#ip_phone"
+    PAIDY_PAYMENT_NEXT_BUTTON = "button#btn_login"
+    PAIDY_PAYMENT_INPUT_PIN_0 = "input#input_pin_0"
+    PAIDY_PAYMENT_INPUT_PIN_1 = "input#input_pin_1"
+    PAIDY_PAYMENT_INPUT_PIN_2 = "input#input_pin_2"
+    PAIDY_PAYMENT_INPUT_PIN_3 = "input#input_pin_3"
+    PAIDY_PAYMENT_SELECT_PAYMENT_OPTIONS = "#root"
+    PAIDY_PAYMENT_SELECT_SINGLE_PAY = "div[testid=npay-select-option-single_pay]"
+    PAIDY_PAYMENT_NEXT_CONFIRM = "button#btn_multi_confirm_next"
 
     PAY_NOW_BUTTON = "button[type=submit]"
 
@@ -138,24 +152,106 @@ module SeleniumServices
         entry_paypal_payment_information()
       elsif @komoju_payment.present?
         entry_komoju_payment_information()
+      elsif @paidy_payment.present?
+        entry_paidy_payment_information()
       end
     end
 
-    def entry_paypal_payment_information
+    def entry_paidy_payment_information
+      email = ''
+      phone_number = ''
+      @paidy_data.each do |conversation|
+        case conversation[:type]
+        when "text_input"
+          puts "-----------------------------conversion: #{conversation[:text_input][:save_input_content]}"
+          case conversation[:text_input][:save_input_content]
+          when "user_email"
+            email = conversation.dig(:text_input, :email_address, :value)
+          when "phone_number"
+            phone_number = conversation.dig(:text_input, :phone_number, :value)
+          end
+      end
+
       @log_tab_level += 1
-      Log.info "entry_paypal_payment_information", @log_tab_level
-      wait_element_load PAYPAL_PAYMENT_CREDIT_CARD_LABEL
-      click PAYPAL_PAYMENT_CREDIT_CARD_LABEL, "Click paypal option"
+      Log.info "entry_paidy_payment_information", @log_tab_level
+      wait_element_load PAIDY_PAYMENT_LABEL
+      click PAIDY_PAYMENT_LABEL, "Click paidy option"
+      click PAY_NOW_BUTTON, "Click pay now button"
 
-      wait_element_load PAYPAL_PAYMENT_CREDIT_CARD_LABEL
-      click PAYPAL_PAYMENT_CREDIT_CARD_LABEL, "Click credit card option"
-
-      # switch to paypal tab
+      # switch to paidy tab
       @driver.switch_to.window driver.window_handles.at(1)
 
       # input email
+      wait_element_load PAIDY_PAYMENT_EMAIL_INPUT
+      element = @driver.find_element(:css, PAIDY_PAYMENT_EMAIL_INPUT)
+      element.clear
+      fill_to_text_input PAIDY_PAYMENT_EMAIL_INPUT, email, "Fill-in user email"
+
+      # input password
+      wait_element_load PAIDY_PAYMENT_PHONE_INPUT
+      fill_to_text_input PAIDY_PAYMENT_PHONE_INPUT, phone_number, "Fill-in phone number"
+
+      # click login
+      wait_element_load PAIDY_PAYMENT_NEXT_BUTTON
+      click PAIDY_PAYMENT_NEXT_BUTTON, "Click login paypal"
+
+      #input pincode
+      pin_code = @pin_code.split('')
+      sleep 60
+      wait_element_load PAIDY_PAYMENT_INPUT_PIN_0
+      fill_to_text_input PAIDY_PAYMENT_INPUT_PIN_0, pin_code[0], "Fill-in pin 0"
+      fill_to_text_input PAIDY_PAYMENT_INPUT_PIN_1, pin_code[1], "Fill-in pin 1"
+      fill_to_text_input PAIDY_PAYMENT_INPUT_PIN_2, pin_code[2], "Fill-in pin 2"
+      fill_to_text_input PAIDY_PAYMENT_INPUT_PIN_3, pin_code[3], "Fill-in pin 3"
+
+      # click checkout
+      wait_element_load PAIDY_PAYMENT_SELECT_SINGLE_PAY
+      click PAIDY_PAYMENT_SELECT_SINGLE_PAY, "Click select single pay"
+
+      #confirm payment
+      wait_element_load PAIDY_PAYMENT_NEXT_CONFIRM
+      click PAIDY_PAYMENT_NEXT_CONFIRM, "Click select single pay"
+
+      wait_element_load PAIDY_PAYMENT_NEXT_CONFIRM
+      click PAIDY_PAYMENT_NEXT_CONFIRM, "Click select single pay"
+
+      @driver.switch_to.default_content
+    end
+
+    def entry_paypal_payment_information
+      email = ''
+      password = ''
+      @paypal_data.each do |conversation|
+        case conversation[:type]
+        when "text_input"
+          puts "-----------------------------conversion: #{conversation[:text_input][:save_input_content]}"
+          case conversation[:text_input][:save_input_content]
+          when "user_email"
+            email = conversation.dig(:text_input, :email_address, :value)
+          when "password"
+            password = conversation.dig(:text_input, :password_confirmation, :value)
+          end
+      end
+
+      @log_tab_level += 1
+      Log.info "entry_paypal_payment_information", @log_tab_level
+      wait_element_load PAYPAL_PAYMENT_LABEL
+      click PAYPAL_PAYMENT_LABEL, "Click paypal option"
+      click PAY_NOW_BUTTON, "Click pay now button"
+      # switch to paypal tab
+      @driver.switch_to.window driver.window_handles.at(1)
+
+      if @driver.find_elements(id: "otpVerification").size() > 0
+        click PAYPAL_PAYMENT_WITH_ANOTHER_METHOD, 'switch to another payment method'
+        wait_element_load PAYPAL_PAYMENT_WITH_ANOTHER_METHOD_BUTTON
+        click PAYPAL_PAYMENT_WITH_ANOTHER_METHOD_BUTTON, 'switch to another payment method button'
+      end
+
+      # input email
       wait_element_load PAYPAL_PAYMENT_EMAIL_INPUT
-      fill_to_text_input PAYPAL_PAYMENT_EMAIL_INPUT, @user_email, "Fill-in user email"
+      element = @driver.find_element(:css, PAYPAL_PAYMENT_EMAIL_INPUT)
+      element.clear
+      fill_to_text_input PAYPAL_PAYMENT_EMAIL_INPUT, email, "Fill-in user email"
 
       # click next
       wait_element_load PAYPAL_PAYMENT_NEXT_BUTTON
@@ -163,7 +259,7 @@ module SeleniumServices
 
       # input password
       wait_element_load PAYPAL_PAYMENT_PASSWORD_INPUT
-      fill_to_text_input PAYPAL_PAYMENT_PASSWORD_INPUT, @password_value, "Fill-in user email"
+      fill_to_text_input PAYPAL_PAYMENT_PASSWORD_INPUT, password, "Fill-in user email"
     
       # click login
       wait_element_load PAYPAL_PAYMENT_LOGIN_BUTTON
@@ -280,6 +376,7 @@ module SeleniumServices
       @password_value = JWT.decode(encrypted_password_value, SECRET_KEY)[0]["data"]
       @country = find_response_by_data_input_name("country")
       @last_name = find_response_by_data_input_name("last_name")
+      @pin_code = find_response_by_data_input_name("pin_code")
       @first_name = find_response_by_data_input_name("first_name")
       @data_address = JSON.parse find_response_by_data_input_name("zip_code_address")
       @post_code = @data_address["value_post_code"].gsub("-", "")
@@ -289,6 +386,8 @@ module SeleniumServices
       @paypal_data = JSON.parse(JWT.decode(@paypal_payment, SECRET_KEY)[0]["data"]) if @paypal_payment.present?
       @komoju_payment = find_response_by_data_input_name("komoju_payment")
       @komoju_data = JSON.parse(JWT.decode(@komoju_payment, SECRET_KEY)[0]["data"]) if @komoju_payment.present?
+      @paidy_payment = find_response_by_data_input_name("paidy_payment")
+      @paidy_data = JSON.parse(@paidy_payment) if @paidy_payment.present?
     end
   end
 end
