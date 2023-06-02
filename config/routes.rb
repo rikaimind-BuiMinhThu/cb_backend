14beba82c6c44146c54f461e06a3cc522a8b4be1
Nguyen Vu Anh Tuan <tuan.nguyen@rikai.technology>
2023-06-02 22:37:35 +0700

    Add ChagLog

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,8 @@ Rails.application.routes.draw do
         get "chatbots/:id/design_settings", :to => 'chatbots#get_design_settings'
         post "chatbots/:id/design_settings", :to => 'chatbots#update_design_settings'
         get "chatbots/:id/sdk", :to => 'chatbots#webchat_sdk'
+        get "chat_log/:bot_id/list", :to => 'chat_log#index'
+        get "chat_log/:sc_id/:user_id", :to => 'chat_log#show'
         get "get_list_chatbot_by_client", :to => 'chatbots#get_list_chatbot_by_client'
         get "get_list_scenario_by_client", :to => 'scenarios#get_list_scenario_by_client'
         resources :user_chatbots, only: [:index, :create, :update, :destroy]
