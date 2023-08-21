2fa7c06f4d04409fcf23b2313ba72d401546b43a
theduc <theduc@duc.nt>
2023-08-21 11:01:52 +0700

    apply code bypass captcha

--- app/services/selenium_services/ec_force.rb
+++ app/services/selenium_services/ec_force.rb
@@ -4,6 +4,10 @@ include Selenium::WebDriver::Keys
 
 module SeleniumServices
   class EcForce < Base
+    CLIENT_KEY = Settings::captcha.client_key
+    WEBSITE_URL = 'https://demo.ec-force.com/shop/products/SStestteiki02'
+    CAPSOLVER_API_CREATE_TASK = 'https://api.capsolver.com/createTask'
+    CAPSOLVER_API_GET_TASK_RESULT = 'https://api.capsolver.com/getTaskResult'
     QUANLITY_INPUT = "input#input-quantity"
     ADD_TO_CART_BUTTON = "button#btn-add"
 
@@ -13,7 +17,9 @@ module SeleniumServices
     # FIRST_NAME_INPUT = "input#order_billing_address_attributes_name02"
     FIRST_NAME_KANA_INPUT = "input#order_billing_address_attributes_kana01"
     # FIRST_NAME_KANA_INPUT = "input#order_billing_address_attributes_kana02"
-    POSTAL_CODE_INPUT = "input#order_billing_address_attributes_zip01"
+    # POSTAL_CODE_INPUT = "input#order_billing_address_attributes_zip01"
+    POSTAL_CODE_INPUT_LEFT = "input#order_billing_address_attributes_zip01"
+    POSTAL_CODE_INPUT_RIGHT = "input#order_billing_address_attributes_zip02"
     ADDRESS_INPUT = "input#order_billing_address_attributes_addr02"
     EMAIL_INPUT = "input#email"
     LOGIN_EMAIL="input#customer_email"
@@ -62,7 +68,8 @@ module SeleniumServices
 
     PAY_NOW_BUTTON = "button[type=submit]"
 
-
+    CHECKBOX_30_INPUT = "input[name='checklist_30']"
+    CHECKBOX_35_INPUT = "input[name='checklist_35']"
 
     def process
       @log_tab_level += 1
@@ -70,13 +77,14 @@ module SeleniumServices
 
       begin
         product_page
-        if @has_account == "0"
+        if @has_account == "1"
           checkout_page
           entry_checkout_information
         else
           checkout_page_regist
           entry_checkout_information_regist
         end
+        bypass_captcha
         confirm_page
         quit
         @selenium_result.update! result: :done, end_time: DateTime.now, last_step_no: @step
@@ -132,7 +140,7 @@ module SeleniumServices
       # capture
       # click CHECKOUT_BUTTON, "Click checkout button"
       # wait_element_load driver.find_element(:css, "a[href*='shop/order/new?register_as_member=0']")
-      @driver.find_element(:css, "a[href*='/shop/order/new?register_as_member=1']").click()
+      @driver.find_element(:css, "a[href*='/shop/order/new?register_as_member=0']").click()
     end
 
     def entry_checkout_information
@@ -143,14 +151,16 @@ module SeleniumServices
       # fill_to_text_input LAST_NAME_INPUT, @last_name, "Fill-in last name"
       fill_to_text_input FIRST_NAME_KANA_INPUT, @first_name_kana, "Fill-in_first_name_kana"
       # fill_to_text_input LAST_NAME_KANA_INPUT, @last_name_kana, "Fill-in_last_name_kana"
-      fill_to_text_input POSTAL_CODE_INPUT, @data_address["value_post_code_left"] + @data_address["value_post_code_right"], "Fill-in postal code"
+      # fill_to_text_input POSTAL_CODE_INPUT, @data_address["value_post_code_left"] + @data_address["value_post_code_right"], "Fill-in postal code"
+      fill_to_text_input POSTAL_CODE_INPUT_LEFT, @data_address["value_post_code_left"], "Fill-in postal code"
+      fill_to_text_input POSTAL_CODE_INPUT_RIGHT, @data_address["value_post_code_right"], "Fill-in postal code"
       fill_to_text_input ADDRESS_INPUT, @data_address["value_address"] + @data_address["value_building_name"], "Fill-in address"
       fill_to_text_input PHONE_NUMBER_INPUT, @phone_number, "Fill-in phonenumber"
       fill_to_text_input EMAIL_INPUT, @user_email, "Fill-in user email"
       fill_to_text_input EMAIL_CONFIRM_INPUT, @user_email, "Fill-in user email"
-      fill_to_text_input PASSWORD_INPUT, password_value, "Fill-in user email"
+      # fill_to_text_input PASSWORD_INPUT, password_value, "Fill-in user email"
       
-      select SELECT_ADDRESS, "same", :select_address
+      # select SELECT_ADDRESS, "same", :select_address
       
       # fill_to_text_input COUPON_CODE, @coupons_code, "Fill-in user email"
       if @np_delivery_payment.present?
@@ -167,6 +177,10 @@ module SeleniumServices
 
         # fill_to_text_input SUBSC_STORE_PAYMENT_SECURITY_CODE_INPUT, card_data["cvc"], :cvc
       end
+      checkbox_30 = @driver.find_element css: CHECKBOX_30_INPUT
+      @driver.execute_script "arguments[0].click();", checkbox_30
+      checkbox_35 = @driver.find_element css: CHECKBOX_35_INPUT
+      @driver.execute_script "arguments[0].click();", checkbox_35
       # select SELECT_PAYMENT_SCHEDULE, "day_of_week", :select_address
       # select SELECT_PAYMENT_SCHEDULE_DATE, (Time.now + @delivery_date.to_i.days).strftime("%Y-%-m-%-d").to_s, :delivery_date
       # select SELECT_PAYMENT_SCHEDULE_TIME, @delivery_time, :delivery_time
@@ -303,6 +317,60 @@ module SeleniumServices
       @sent_message = find_response_by_data_input_name("sent_message")
       encrypted_password_value = find_response_by_data_input_name("user_password")
       @password_value = JWT.decode(encrypted_password_value, SECRET_KEY)[0]["data"] if encrypted_password_value.present?
+    end
+
+    def bypass_captcha
+      @log_tab_level += 1
+      Log.info "bypass_captcha", @log_tab_level
+      text = @driver.page_source
+      key_match = text.scan(/"key":"([^"]+)"/)
+      iv_match = text.scan(/"iv":"([^"]+)"/)
+      context_match = text.scan(/"context":"([^"]+)"/)
+      challenge_match = text.scan(/<script.*?src="(.*?)".*?><\/script>/)
+
+      if key_match && iv_match && context_match
+        awsKey = key_match.flatten.first
+        awsIv = iv_match.flatten.first
+        awsContext = context_match.flatten.first
+        awsChallengeJS = challenge_match.flatten.first
+      end
+
+      params_create_task = {
+        'clientKey': CLIENT_KEY,
+        'task': {
+          'type': 'AntiAwsWafTaskProxyless',
+          'websiteURL': WEBSITE_URL,
+          'awsKey': awsKey,
+          'awsIv': awsIv,
+          'awsContext': awsContext,
+          'awsChallengeJS': awsChallengeJS
+        }
+      }
+      Log.info "call API createTask", @log_tab_level
+      request_create_task = HttpManager.new(CAPSOLVER_API_CREATE_TASK, params_create_task)
+      response_create_task = request_create_task.post_request
+      sleep 5
+      taskId = response_create_task["taskId"]
+
+      params_get_task_result = {
+        'clientKey': CLIENT_KEY,
+        'taskId': taskId
+      }
+
+      Log.info "call API getTaskResult", @log_tab_level
+      request_get_task_result = HttpManager.new(CAPSOLVER_API_GET_TASK_RESULT, params_get_task_result)
+      response_get_task_result = request_get_task_result.post_request
+      sleep 5
+
+      if response_get_task_result["status"] == "ready"
+        all_cookies = @driver.manage.all_cookies
+        target_cookie = all_cookies.find { |cookie| cookie[:name] == 'aws-waf-token' }
+        target_cookie[:value] = response_get_task_result["solution"]["cookie"]
+        @driver.manage.delete_cookie(target_cookie[:name])
+        @driver.manage.add_cookie(target_cookie)
+        @driver.navigate.refresh
+      end
+      capture
     end
   end
 end
