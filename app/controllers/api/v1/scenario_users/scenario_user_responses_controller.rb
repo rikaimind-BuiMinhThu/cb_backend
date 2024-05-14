b83c961a06a6212b65d92651b9021b86f2591900
minhdt <minhdt@gmail.com>
2024-05-14 16:19:27 +0700

    update

--- app/controllers/api/v1/scenario_users/scenario_user_responses_controller.rb
+++ app/controllers/api/v1/scenario_users/scenario_user_responses_controller.rb
@@ -7,9 +7,6 @@ class Api::V1::ScenarioUsers::ScenarioUserResponsesController < ApplicationContr
     if !!@client && (@client.tamago_repeat? || @client.shopify? || @client.subsc_store? || @client.repeat_plus?) && params[:user_id].present?
       scenario_user_responses = ScenarioUserResponse.build_record(params)
       scenario_user_responses.each(&:save!) if scenario_user_responses.present?
-      if @client.shopify? && scenario_user_responses.present?
-        return render json: { code: 1, data: scenario_user_responses, message: 'controller' }
-      end
       render json: { code: 1, data: scenario_user_responses }
     elsif !!@client && (@client.ec_force?) && params[:user_id].present?
       scenario_user_responses = ScenarioUserResponse.build_record(params)
