90b2c5edb8ddb91f29ebac8c6281261a47fbd136
DucAnhRikai <anh.nguyen3@rikai.technology>
2025-04-09 08:16:16 +0700

    (Refactor) Controller

--- app/controllers/api/v1/scenario_users/conversations_controller.rb
+++ app/controllers/api/v1/scenario_users/conversations_controller.rb
@@ -6,24 +6,22 @@ class Api::V1::ScenarioUsers::ConversationsController < ApplicationController
     params_to_check = %i[scenario_id client_id bot_type user_input_id]
 
     params_to_check.each do |param|
-      if params[param].to_s.strip.empty?
-        render json: { code: 2, message: "#{param} is required!" }, status: :bad_request and return
+      render json: { code: 2, message: "#{param} is required!" } and return if params[param].to_s.strip.empty?
+
+      begin
+        @order = Order.create!(
+          scenario_id: params[:scenario_id],
+          bot_type: params[:bot_type],
+          client_id: params[:client_id],
+          user_input_id: params[:user_input_id]
+        )
+
+        render json: { code: 1, message: 'Order created successfully' }
+      rescue StandardError => e
+        render json: { code: 2, message: e.message }
+
+        Rails.logger.error "An error occurred: #{e.message}"
       end
-    end
-
-    begin
-      @order = Order.create!(
-        scenario_id: params[:scenario_id],
-        bot_type: params[:bot_type],
-        client_id: params[:client_id],
-        user_input_id: params[:user_input_id]
-      )
-
-      render json: { code: 1, message: 'Order created successfully' }, status: :ok
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { code: 2, message: e.message }, status: :internal_server_error
-    rescue StandardError => e
-      render json: { code: 2, message: e.message }, status: :internal_server_error
     end
   end
 end
