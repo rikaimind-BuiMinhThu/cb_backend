5b69662b315a12f42c0f5465f315a5891a8d3a1a
ThuBui <buiminhthuk55@gmail.com>
2025-04-09 12:11:03 +0700

    Refactor

--- app/controllers/api/v1/scenario_users/conversations_controller.rb
+++ app/controllers/api/v1/scenario_users/conversations_controller.rb
@@ -3,20 +3,10 @@ class Api::V1::ScenarioUsers::ConversationsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    permitted = order_params
-
-    # Validate presence of required parameters
-    required_params = %i[scenario_id bot_type client_id user_input_id]
-    missing_params = required_params.select { |param| permitted[param].blank? }
-
-    if missing_params.any?
-      return render json: { message: "#{missing_params.join(', ')} #{missing_params.size > 1 ? 'are' : 'is'} required!" },
-                    status: :bad_request
-    end
-
-    # Create Order with the permitted parameters
-    @order = Order.create!(permitted)
+    Order.create!(order_params.merge({client_id: client_id}))
     render json: { message: 'Order created successfully' }, status: :ok
+  rescue ActionController::ParameterMissing => e
+    render json: { error: e.message }, status: :bad_request
   rescue StandardError => e
     Rails.logger.error "An error occurred: #{e.message}"
     render json: { message: e.message }, status: :internal_server_error
@@ -24,8 +14,16 @@ class Api::V1::ScenarioUsers::ConversationsController < ApplicationController
 
   private
 
+  def client_id
+    scenario = Scenario.find_by(id: params[:scenario_id])
+    chatbot = Chatbot.find_by(id: params[:bot_id])
+    scenario&.chatbot&.user&.client_id
+  end
+
   # Only allow required fields
   def order_params
-    params.permit(:scenario_id, :bot_type, :client_id, :user_input_id)
+    params.require(:scenario_id)
+    params.require(:bot_type)
+    params.require(:user_input_id)
   end
 end
