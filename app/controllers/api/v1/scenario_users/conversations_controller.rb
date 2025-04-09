84d74c07a33d31e6af16ea38813a6658a889b03f
DucAnhRikai <anh.nguyen3@rikai.technology>
2025-04-09 09:11:12 +0700

    (Refactor) Controller: Loop remove

--- app/controllers/api/v1/scenario_users/conversations_controller.rb
+++ app/controllers/api/v1/scenario_users/conversations_controller.rb
@@ -6,22 +6,25 @@ class Api::V1::ScenarioUsers::ConversationsController < ApplicationController
     params_to_check = %i[scenario_id client_id bot_type user_input_id]
 
     params_to_check.each do |param|
-      render json: { code: 2, message: "#{param} is required!" } and return if params[param].to_s.strip.empty?
-
-      begin
-        @order = Order.create!(
-          scenario_id: params[:scenario_id],
-          bot_type: params[:bot_type],
-          client_id: params[:client_id],
-          user_input_id: params[:user_input_id]
-        )
-
-        render json: { code: 1, message: 'Order created successfully' }
-      rescue StandardError => e
-        render json: { code: 2, message: e.message }
-
-        Rails.logger.error "An error occurred: #{e.message}"
+      if params[param].to_s.strip.empty?
+        render json: { message: "#{param} is required!" },
+               status: :bad_request and return
       end
+    end
+
+    begin
+      @order = Order.create!(
+        scenario_id: params[:scenario_id],
+        bot_type: params[:bot_type],
+        client_id: params[:client_id],
+        user_input_id: params[:user_input_id]
+      )
+
+      render json: { message: 'Order created successfully' }, status: :ok
+    rescue StandardError => e
+      render json: { message: e.message }, status: :internal_server_error
+
+      Rails.logger.error "An error occurred: #{e.message}"
     end
   end
 end
