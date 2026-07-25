bcd05061f21e643b749ed9dc385b85919f7f174c
ThuBui <buiminhthuk55@gmail.com>
2026-07-26 01:13:54 +0900

    Fix bug: Can not delete message bag and message group

--- app/forms/message_group_form.rb
+++ app/forms/message_group_form.rb
@@ -1,47 +1,38 @@
 class MessageGroupForm
   include ActiveModel::Model
 
+  GROUP_PREFIX = "グループ内のメッセージ袋が".freeze
+
   attr_reader :message_group
   attr_reader :current_user
 
   def initialize(message_group, current_user)
     @message_group = message_group
-    @instagram_account = current_user.instagram_account
+    @current_user = current_user
   end
 
   def check_delete
+    delete_block_reason.nil?
+  end
+
+  def delete_block_reason
     list_bag_ids = @message_group.message_bags.pluck(:id)
-    return true if list_bag_ids.blank?
+    return nil if list_bag_ids.blank?
 
     list_bag_ids.each do |bag_id|
-      return false if globally_referenced?(bag_id)
+      reason = bag_block_reason(bag_id)
+      return "#{GROUP_PREFIX}#{reason}" if reason.present?
     end
 
-    return true if @instagram_account.blank?
-
-    referenced_ids = account_referenced_bag_ids
-    list_bag_ids.each do |bag_id|
-      return false if referenced_ids.include?(bag_id)
-    end
-
-    true
+    nil
   end
 
   private
 
-  def globally_referenced?(bag_id)
-    PersistentMenu.where(message_bag_id: bag_id).exists? ||
-      IceBreaker.where(message_bag_id: bag_id).exists? ||
-      MessageButton.where(message_bag_id: bag_id).exists? ||
-      Conversion.where(message_bag_id: bag_id).exists?
-  end
+  def bag_block_reason(bag_id)
+    bag = MessageBag.find_by(id: bag_id)
+    return nil if bag.blank?
 
-  def account_referenced_bag_ids
-    ids = KeywordSetting.where(instagram_account_id: @instagram_account.id).pluck(:message_bag_id)
-    ids << @instagram_account.post_comment_bag_id if @instagram_account.post_comment_bag_id.present?
-    ids << @instagram_account.story_comment_bag_id if @instagram_account.story_comment_bag_id.present?
-    ids << @instagram_account.live_comment_bag_id if @instagram_account.live_comment_bag_id.present?
-    ids << @instagram_account.default_reply_bag_id if @instagram_account.default_reply_bag_id.present?
-    ids.compact
+    MessageBagForm.new(bag, @current_user).delete_block_reason
   end
 end
